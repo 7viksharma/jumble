55d3f58975b8fbc5dbe35c1d1464a3b8ce5152d3
Satvik Sharma <7viksharma@gmail.com>
2022-03-04 20:19:12 +0530

    new branch

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,8 @@
 import React from "react";
+import { Login } from "./pages/Login";
 
 function App() {
-  return <div className="App">hello</div>;
+  return <Login></Login>;
 }
 
 export default App;
